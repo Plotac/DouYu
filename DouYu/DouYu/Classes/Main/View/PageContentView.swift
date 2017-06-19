d501e5e3b9f1e111421d4ebc02096d7fd7610ca1
Plo <448950706@qq.com>
2017-06-19 18:17:08 +0800

    ...

---
+++ DouYu/DouYu/Classes/Main/View/PageContentView.swift
@@ -0,0 +1,168 @@
+//
+//  PageContentView.swift
+//  DouYu
+//
+//  Created by Plo on 2017/6/15.
+//  Copyright © 2017年 Plo. All rights reserved.
+//
+
+import UIKit
+
+protocol PageContentViewDelegate : class {
+    func pageContentView(pageContentView: PageContentView, progress: CGFloat, sourceIndex: Int, targetIndex: Int)
+}
+
+
+private let kContentCellID : String = "ContentCell"
+
+class PageContentView: UIView {
+    // Mark: - 定义属性
+    fileprivate var childVCs : [UIViewController]
+    fileprivate weak var parentVC : UIViewController?
+    fileprivate var startOffsetX : CGFloat = 0
+    weak var delegate : PageContentViewDelegate?
+    fileprivate var isForbitScrollDelegate : Bool = false
+
+    // Mark: - 懒加载属性
+    //CollectionView
+    fileprivate lazy var collectionView : UICollectionView = { [weak self] in
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = (self?.bounds.size)!
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.scrollDirection = .horizontal
+        
+        let coll = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        coll.showsHorizontalScrollIndicator = false
+        coll.isPagingEnabled = true
+        coll.bounces = false
+        coll.dataSource = self
+        coll.delegate = self
+        
+        coll.register(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: kContentCellID)
+        
+        return coll
+    }()
+    
+    init(frame: CGRect,childVCs: [UIViewController],parentVC: UIViewController?) {
+        
+        self.childVCs = childVCs
+        self.parentVC = parentVC
+        
+        super.init(frame: frame)
+        
+        setUpUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// Mark: - 设置UI
+extension PageContentView {
+    
+    fileprivate func setUpUI() {
+        //1.将所有的子控制器添加到父控制器中
+        for childVC in self.childVCs {
+            parentVC?.addChildViewController(childVC)
+        }
+        //2.设置UICollectionView，用于在cell中存放控制器的view
+        addSubview(collectionView)
+        collectionView.frame = bounds
+    }
+
+}
+
+// Mark: - CollectionViewDataSource
+extension PageContentView : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return childVCs.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kContentCellID, for: indexPath)
+        
+        for view in cell.contentView.subviews {
+            view.removeFromSuperview()
+        }
+        
+        let childVC = childVCs[indexPath.item]
+        childVC.view.frame = cell.bounds
+        cell.contentView.addSubview(childVC.view)
+        
+        return cell
+    }
+
+}
+
+// Mark: - CollectionViewDelegate
+extension PageContentView : UICollectionViewDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        
+        isForbitScrollDelegate = false
+        
+        startOffsetX = scrollView.contentOffset.x
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //判断是否是点击事件
+        if isForbitScrollDelegate { return }
+        
+        var progress : CGFloat = 0
+        var sourcesIndex : Int = 0
+        var targetIndex : Int = 0
+        
+        let currentOffset : CGFloat = scrollView.contentOffset.x
+        let scrollViewW : CGFloat = scrollView.bounds.width
+
+        if currentOffset < startOffsetX {//左滑
+            
+            //左滑：currentOffset增加 所以currentOffset / scrollViewW 是大于0的小数
+            //floor()函数 : 取整
+            progress = currentOffset / scrollViewW - floor(currentOffset / scrollViewW)// x.y - x = 0.y
+            
+            sourcesIndex = Int(currentOffset / scrollViewW)
+            
+            targetIndex = sourcesIndex + 1
+            if targetIndex >= childVCs.count {//防止越界
+                targetIndex = childVCs.count - 1
+            }
+            
+            //如果完全滑过去
+            if currentOffset - startOffsetX == scrollViewW {
+                progress = 1
+                targetIndex = sourcesIndex
+            }
+            
+            
+        }else {//右滑
+            //右滑：currentOffset减少 所以currentOffset / scrollViewW 是大于0小于1的小数
+            progress = 1 - (currentOffset / scrollViewW - floor(currentOffset / scrollViewW))
+            
+            targetIndex = Int(currentOffset / scrollViewW)
+            
+            sourcesIndex = targetIndex + 1
+            if sourcesIndex >= childVCs.count {//防止越界
+                sourcesIndex = childVCs.count - 1
+            }
+        
+        }
+        
+        delegate?.pageContentView(pageContentView: self, progress: progress, sourceIndex: sourcesIndex, targetIndex: targetIndex)
+        
+    }
+}
+
+// Mark: - 对外暴露的方法
+extension PageContentView {
+    
+    func setContentViewCurrentIndex(index: Int) {
+        //如果是滑动的话，禁止代理方法
+        isForbitScrollDelegate = true
+        
+        let offsetX = CGFloat(index) * collectionView.frame.width
+        collectionView.setContentOffset(CGPoint(x:offsetX,y:0), animated: false)
+        
+    }
+}
---
+++ DouYu/DouYu/Classes/Main/View/PageTitleView.swift
@@ -0,0 +1,184 @@
+//
+//  PageTitleView.swift
+//  DouYu
+//
+//  Created by Plo on 2017/6/14.
+//  Copyright © 2017年 Plo. All rights reserved.
+//
+
+import UIKit
+
+//Mark: - 定义常量
+private let kScrollLineH : CGFloat = 2
+private let kNormalColor : (CGFloat,CGFloat,CGFloat) = (85,85,85)
+private let kSelectColor : (CGFloat,CGFloat,CGFloat) = (255,128,0)
+
+//Mark: - 定义协议
+protocol PageTitleViewDelegate : class{
+    func pagetTitleView(titleView: PageTitleView,selectIndex: Int)
+}
+
+//Mark: - 定义PageTitleView类
+class PageTitleView : UIView {
+    
+    //定义属性
+    //标题数组
+    fileprivate var titles : [String]
+    fileprivate var currentIndex : Int = 0
+    weak var delegate : PageTitleViewDelegate?
+    
+    
+    //懒加载属性
+    fileprivate lazy var titleLabels : [UILabel] = [UILabel]()
+    //滚动视图
+    fileprivate lazy var scrollView : UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.showsHorizontalScrollIndicator = false
+        scroll.scrollsToTop = false
+        scroll.bounces = false
+        return scroll
+    }()
+    //滑块
+    fileprivate lazy var scrollLine : UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+        return line
+    }()
+    
+    // Mark: - 自定义构造函数
+    init(frame: CGRect , titles: [String]) {
+        
+        self.titles = titles;
+        super.init(frame: frame)
+        
+        setUpUI()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+//Mark: - 设置UI
+extension PageTitleView {
+
+    fileprivate func setUpUI() {
+        //添加ScrollView
+        addSubview(scrollView)
+        scrollView.frame = bounds
+        
+        //创建label
+        setUpTitleLabels()
+        
+        //创建滑块和底线
+        setUpBottomLineAndScrollLine()
+    }
+    
+    private func setUpTitleLabels() {
+        let labelW : CGFloat = frame.width/CGFloat(self.titles.count)
+        let labelH : CGFloat = frame.height - kScrollLineH
+        let labelY : CGFloat = 0.0
+        
+        for (index,title) in self.titles.enumerated() {
+            let  label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 16.0)
+            label.textAlignment = .center
+            label.textColor = UIColor(r: kNormalColor.0, g: kNormalColor.1, b: kNormalColor.2)
+            label.tag = index
+            label.text = title
+            label.isUserInteractionEnabled = true
+            
+            let labelX = CGFloat(index) * labelW
+            label.frame = CGRect(x: labelX, y: labelY, width: labelW, height: labelH)
+            scrollView.addSubview(label)
+            titleLabels.append(label)
+            
+            let tapGes = UITapGestureRecognizer(target: self, action: #selector(self.labelClick(tap:)))
+            label.addGestureRecognizer(tapGes)
+        }
+    }
+
+    private func setUpBottomLineAndScrollLine() {
+    
+        //底部横线
+        let bottomLine = UIView()
+        let lineH : CGFloat = 0.5
+        
+        bottomLine.frame = CGRect(x: 0, y: frame.height-lineH, width: frame.size.width, height: lineH)
+        bottomLine.backgroundColor = UIColor.lightGray
+        addSubview(bottomLine)
+        
+        //滑块
+        scrollView.addSubview(scrollLine)
+        guard let firstLabel = titleLabels.first else { return }
+        firstLabel.textColor = UIColor.orange
+        scrollLine.frame = CGRect(x: 10, y: frame.height-kScrollLineH, width: firstLabel.frame.width-10*2, height: kScrollLineH)
+        
+    }
+}
+
+//Mark: - 监听label点击事件
+extension PageTitleView {
+
+    @objc fileprivate func labelClick(tap : UITapGestureRecognizer) {
+        //1.拿到点击之后的当前label
+        guard let currentLabel = tap.view as? UILabel else { return }
+        currentLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+        
+        //2.点击之前的旧label
+        let oldLabel = titleLabels[currentIndex]
+        
+        if currentIndex == currentLabel.tag {
+            currentLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+            oldLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+        }else {
+            oldLabel.textColor = UIColor(r: kNormalColor.0, g: kNormalColor.1, b: kNormalColor.2)
+        }
+    
+       
+        
+        currentIndex = currentLabel.tag
+        
+        //3.滑块位置改变
+        let linePositionX = currentLabel.frame.width * CGFloat(currentLabel.tag) + 10
+        UIView.animate(withDuration: 0.15) {
+            self.scrollLine.frame.origin.x = linePositionX
+        }
+        
+        delegate?.pagetTitleView(titleView: self, selectIndex: currentIndex)
+    
+    }
+}
+
+//Mark: - 对外暴露的方法
+extension PageTitleView {
+    func setTitlesWith(progress: CGFloat,sourceIndex: Int,targetIndex: Int) {
+
+        //取出sourceLabel和targetLabel
+        let sourceLabel = titleLabels[sourceIndex]
+        let targetLabel = titleLabels[targetIndex]
+        
+        //滑块渐变
+        let moveTotalX = targetLabel.frame.origin.x - sourceLabel.frame.origin.x
+        let moveX = moveTotalX * progress
+        scrollLine.frame.origin.x = sourceLabel.frame.origin.x + 10 + moveX
+        
+        //颜色渐变
+        //1.颜色变化的总范围
+        let colorRange = (kSelectColor.0 - kNormalColor.0,kSelectColor.1 - kNormalColor.1,kSelectColor.2 - kNormalColor.2)
+        
+        //2.变化sourceLabel颜色
+        sourceLabel.textColor = UIColor(r: kSelectColor.0 - colorRange.0 * progress, g: kSelectColor.1 - colorRange.1 * progress, b: kSelectColor.2 - colorRange.2 * progress)
+        
+        //3.变化targetLabel颜色
+        targetLabel.textColor = UIColor(r: kNormalColor.0 + colorRange.0 * progress, g: kNormalColor.1 + colorRange.1 * progress, b: kNormalColor.2 + colorRange.2 * progress)
+        
+        //4.记录最新的Index
+        currentIndex = targetIndex
+        
+        
+    }
+}
